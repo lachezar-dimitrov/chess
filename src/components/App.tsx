ce193eff006e61ce4b1d7046ce997ef76a19a8a9
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2021-11-07 11:12:40 +0200

    Improve App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
+import React, { Component, ReactNode } from 'react';
 import { observer } from 'mobx-react';
-import Game from './Game';
-import AppStore from '../store/app';
-
-type Props = {
-  store: AppStore;
-}
+import Game from './Game/Game';
 
 @observer
-export default class App extends React.Component<Props> {
-  render(): JSX.Element {
-    const { store } = this.props;
-    return (
-      <div className="game-container">
-        <Game store={store} />
-      </div>
-    );
+export default class App extends Component {
+  render(): ReactNode {
+    return <Game />;
   }
 }
