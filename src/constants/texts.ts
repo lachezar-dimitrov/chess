f9bb9849c1aa984546e9095d69d958f96926ea5d
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2021-11-10 15:18:32 +0200

    Add new texts

--- src/constants/texts.ts
+++ src/constants/texts.ts
@@ -5,3 +5,5 @@ export const RULES = `You probably already know how to play Tic-Tac-Toe.
     It's a really simple game, right? That's what most people think.
     But if you really wrap your brain around it,
     you'll discover that Tic-Tac-Toe isn't quite as simple as you think!`;
+export const NEXT_PLAYER = 'Next player';
+export const DRAW_MESSAGE = 'Draw';
