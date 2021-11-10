d2dc92ccf4173d3ff1942753a3e275e23965d9e1
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2021-11-10 07:52:08 +0200

    Add texts

---
+++ src/constants/texts.ts
@@ -0,0 +1,7 @@
+export const WINNER_MESSAGE = 'The winner is';
+export const X = 'X';
+export const O = 'O';
+export const RULES = `You probably already know how to play Tic-Tac-Toe.
+    It's a really simple game, right? That's what most people think.
+    But if you really wrap your brain around it,
+    you'll discover that Tic-Tac-Toe isn't quite as simple as you think!`;
