85bf39a7fc0f2fe3ecf277f4b0e35fba05c9fbf8
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2021-11-19 09:53:55 +0200

    Setup test libs

--- config/paths.js
+++ config/paths.js
@@ -63,7 +63,7 @@ module.exports = {
   appTsConfig: resolveApp('tsconfig.json'),
   appJsConfig: resolveApp('jsconfig.json'),
   yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveModule(resolveApp, 'src/setupTests'),
+  testsSetup: resolveModule(resolveApp, 'src/tests/setupTests'),
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
