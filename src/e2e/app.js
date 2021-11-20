6807e36fd199419ad288e02fbe8fcf4defa05b86
lachezar-dimitrov <lachezar.dimitrov@atscale.com>
2021-11-20 18:18:26 +0200

    Add e2e tests

--- src/e2e/app.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'Open the app'(browser) {
-        browser
-        .url('http://localhost:3000/')
-        .waitForElementVisible('.hello')
-        .assert
-        .containsText('.hello','Hello World');
-    }
-};
